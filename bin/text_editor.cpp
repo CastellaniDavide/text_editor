3069b8a95fdceebd8481158f2efb48b4eed17151
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-03 15:14:29 +0200

    Created bin/template.cpp

---
+++ bin/text_editor.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file text_editor.cpp
+ *
+ * @version 01.01 2020-9-3
+ *
+ * @brief https://training.olinfo.it/#/task/ois_editor/statement
+ *
+ * @ingroup text_editor
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+//  uncomment the following lines if you want to read/write from files
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  cin >> N;
+
+  return 0;
+}
